8d86e6d30b6aeb24e08c23876958a392f516d44d
Felix Springer <felixspringer149@gmail.com>
2023-02-28 01:16:16 +0100

    Remove dead code, that has been found by weeder.

--- server/source/library/Text/Blaze/Html5/Extra.hs
+++
@@ -1,8 +0,0 @@
-module Text.Blaze.Html5.Extra where
-
-import Text.Blaze.Html5
-import Text.Blaze.Internal
-
--- | This should be exported from `Text.Blaze.Html5`, but isn't.
-s :: Html -> Html
-s = Parent "s" "<s" "</s>"
