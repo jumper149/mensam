746b7ca45c8004c9ccaf04c891a8c6782d19cbab
Felix Springer <felixspringer149@gmail.com>
2023-02-28 11:31:41 +0100

    Add orphan instances for all of the monad-logger library.

--- server/source/library/Control/Monad/Logger/OrphanInstances.hs
+++ server/source/library/Control/Monad/Logger/OrphanInstances.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE MonoLocalBinds #-}
+{-# LANGUAGE UndecidableInstances #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Control.Monad.Logger.OrphanInstances () where
@@ -32,3 +34,35 @@ deriving via
   LoggingT ((t2 :: (Type -> Type) -> Type -> Type) m)
   instance
     MonadIO (t2 m) => MonadLogger (ComposeT LoggingT t2 m)
+
+deriving via
+  NoLoggingT ((t2 :: (Type -> Type) -> Type -> Type) m)
+  instance
+    MonadIO (t2 m) => MonadLogger (ComposeT NoLoggingT t2 m)
+
+deriving via
+  WriterLoggingT ((t2 :: (Type -> Type) -> Type -> Type) m)
+  instance
+    Monad (t2 m) => MonadLogger (ComposeT WriterLoggingT t2 m)
+
+instance (Monad (t m), MonadTrans t, MonadLoggerIO m) => MonadLoggerIO (Elevator t m) where
+  askLoggerIO = lift askLoggerIO
+
+deriving via
+  Elevator t1 (t2 (m :: Type -> Type))
+  instance
+  {-# OVERLAPPABLE #-}
+    ( Monad (t1 (t2 m))
+    , MonadTrans t1
+    , MonadLoggerIO (t2 m)
+    , MonadIO (ComposeT t1 t2 m)
+    ) =>
+    MonadLoggerIO (ComposeT t1 t2 m)
+
+deriving via
+  LoggingT (t2 (m :: Type -> Type))
+  instance
+    ( MonadIO (t2 m)
+    , MonadIO (ComposeT LoggingT t2 m)
+    ) =>
+    MonadLoggerIO (ComposeT LoggingT t2 m)
