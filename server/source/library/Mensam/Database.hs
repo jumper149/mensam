9ba9c74612234ac4ab6e560f589f11e0f7a418e0
Felix Springer <felixspringer149@gmail.com>
2023-03-05 01:09:28 +0100

    Add "spaces" which will be environments for "tables".

--- server/source/library/Mensam/Database.hs
+++ server/source/library/Mensam/Database.hs
@@ -29,6 +29,45 @@ userTable =
     ]
     (fromJust . T.stripPrefix "dbUser_")
 
+type DbSpace :: Type
+data DbSpace = MkDbSpace
+  { dbSpace_id :: Selda.ID DbSpace
+  , dbSpace_name :: Selda.Text
+  }
+  deriving stock (Generic, Show)
+  deriving anyclass (Selda.SqlRow)
+
+spaceTable :: Selda.Table DbSpace
+spaceTable =
+  Selda.tableFieldMod
+    "space"
+    [ #dbSpace_id Selda.:- Selda.autoPrimary
+    , #dbSpace_name Selda.:- Selda.unique
+    ]
+    (fromJust . T.stripPrefix "dbSpace_")
+
+type DbSpaceUser :: Type
+data DbSpaceUser = MkDbSpaceUser
+  { dbSpaceUser_id :: Selda.ID DbSpaceUser
+  , dbSpaceUser_space :: Selda.ID DbSpace
+  , dbSpaceUser_user :: Selda.ID DbUser
+  }
+  deriving stock (Generic, Show)
+  deriving anyclass (Selda.SqlRow)
+
+spaceUserTable :: Selda.Table DbSpaceUser
+spaceUserTable =
+  Selda.tableFieldMod
+    "space_user"
+    [ #dbSpaceUser_id Selda.:- Selda.autoPrimary
+    , #dbSpaceUser_space Selda.:- Selda.foreignKey spaceTable #dbSpace_id
+    , #dbSpaceUser_user Selda.:- Selda.foreignKey userTable #dbUser_id
+    , #dbSpaceUser_space Selda.:+ #dbSpaceUser_user Selda.:- Selda.unique
+    ]
+    (fromJust . T.stripPrefix "dbSpaceUser_")
+
 initDatabase :: MonadSeldaConnection m => m ()
 initDatabase = runSeldaTransaction $ do
   Selda.createTable userTable
+  Selda.createTable spaceTable
+  Selda.createTable spaceUserTable
