9f7231b0a66a02279071b5da134f268a67bfb7e7
Felix Springer <felixspringer149@gmail.com>
2023-03-07 20:22:49 +0100

    Add convenience functionality for Selda.
    This include `queryUnique` and `queryOne`.

---
+++ server/source/library/Mensam/Database/Extra.hs
@@ -0,0 +1,19 @@
+module Mensam.Database.Extra where
+
+import Control.Monad.Catch
+import Database.Selda
+
+-- | Run 'query', but throw an error when there are multiple results.
+queryUnique :: (MonadSelda m, MonadThrow m, Result a) => Query (Backend m) a -> m (Maybe (Res a))
+queryUnique q =
+  query q >>= \case
+    _ : _ : _ -> throwM $ UnsafeError "Expected unique result, but the query returned multiple results."
+    [spaceId] -> pure $ Just spaceId
+    [] -> pure Nothing
+
+-- | Run 'query', but throw an error unless there is exactly on result.
+queryOne :: (MonadSelda m, MonadThrow m, Result a) => Query (Backend m) a -> m (Res a)
+queryOne q =
+  queryUnique q >>= \case
+    Just spaceId -> pure spaceId
+    Nothing -> throwM $ UnsafeError "Expected exactly one result, but the query didn't return any results."
