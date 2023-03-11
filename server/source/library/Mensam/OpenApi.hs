1b2b3e8a672389f1f48625e274f51ed459d67d7f
Felix Springer <felixspringer149@gmail.com>
2023-03-11 15:53:20 +0100

    Create separate module `Servant.Auth.OrphanInstances`.

--- server/source/library/Mensam/OpenApi.hs
+++ server/source/library/Mensam/OpenApi.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE GADTs #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Mensam.OpenApi where
@@ -12,17 +11,14 @@ import Mensam.Server.Route.User.Type qualified as User
 import Mensam.User.Username
 
 import Control.Lens
-import Data.HashMap.Strict.InsOrd qualified as HM
-import Data.Kind
 import Data.OpenApi
 import Data.OpenApi qualified as OpenApi (pattern)
 import Data.Proxy
-import Data.Text qualified as T
 import Data.Typeable
 import Deriving.Aeson qualified as A
 import Deriving.Aeson.OrphanInstances qualified as A ()
 import Servant.API
-import Servant.Auth qualified
+import Servant.Auth.OrphanInstances ()
 import Servant.OpenApi
 import Servant.RawM
 import Text.Blaze.Html qualified as Blaze
@@ -37,86 +33,6 @@ instance ToSchema OpenApi where
 -- TODO: Implement.
 instance ToSchema Blaze.Markup where
   declareNamedSchema Proxy = declareNamedSchema $ Proxy @()
-
-type AuthMethodList :: [Type] -> Type
-data AuthMethodList xs :: Type where
-  MethodBasicAuth :: AuthMethodList auths -> AuthMethodList (Servant.Auth.BasicAuth ': auths)
-  MethodJWT :: AuthMethodList auths -> AuthMethodList (Servant.Auth.JWT ': auths)
-  MethodCookie :: AuthMethodList auths -> AuthMethodList (Servant.Auth.Cookie ': auths)
-  MethodNone :: AuthMethodList '[]
-
-type AuthMethods :: [Type] -> Constraint
-class AuthMethods auths where
-  authMethods :: Proxy auths -> AuthMethodList auths
-
-instance AuthMethods '[] where
-  authMethods Proxy = MethodNone
-
-instance AuthMethods auths => AuthMethods (Servant.Auth.BasicAuth ': auths) where
-  authMethods Proxy = MethodBasicAuth $ authMethods Proxy
-
-instance AuthMethods auths => AuthMethods (Servant.Auth.JWT ': auths) where
-  authMethods Proxy = MethodJWT $ authMethods Proxy
-
-instance AuthMethods auths => AuthMethods (Servant.Auth.Cookie ': auths) where
-  authMethods Proxy = MethodCookie $ authMethods Proxy
-
-addAuthMethods :: AuthMethodList auths -> OpenApi -> OpenApi
-addAuthMethods = \case
-  MethodBasicAuth nextMethod ->
-    let
-      identifier :: T.Text = "BasicAuth"
-      addThisScheme =
-        addSecurityScheme identifier $
-          SecurityScheme
-            { _securitySchemeType = SecuritySchemeHttp HttpSchemeBasic
-            , _securitySchemeDescription = Just "Basic Authentication"
-            }
-      addThisRequirement = addSecurityRequirement $ SecurityRequirement $ HM.singleton identifier []
-     in
-      addAuthMethods nextMethod . addThisRequirement . addThisScheme
-  MethodJWT nextMethod ->
-    let
-      identifier :: T.Text = "JWT"
-      addThisScheme =
-        addSecurityScheme identifier $
-          SecurityScheme
-            { _securitySchemeType = SecuritySchemeHttp $ HttpSchemeBearer $ Just "JWT"
-            , _securitySchemeDescription = Just "Bearer Authentication"
-            }
-      addThisRequirement = addSecurityRequirement $ SecurityRequirement $ HM.singleton identifier []
-     in
-      addAuthMethods nextMethod . addThisRequirement . addThisScheme
-  MethodCookie nextMethod ->
-    let
-      identifier :: T.Text = "Cookie"
-      addThisScheme =
-        addSecurityScheme identifier $
-          SecurityScheme
-            { _securitySchemeType = SecuritySchemeHttp $ HttpSchemeBearer $ Just "JWT"
-            , _securitySchemeDescription = Just "Cookie Authentication"
-            }
-      addThisRequirement = addSecurityRequirement $ SecurityRequirement $ HM.singleton identifier []
-     in
-      addAuthMethods nextMethod . addThisRequirement . addThisScheme
-  MethodNone -> id
-
-addSecurityScheme :: T.Text -> SecurityScheme -> OpenApi -> OpenApi
-addSecurityScheme securityIdentifier securityScheme openApi =
-  openApi
-    { _openApiComponents =
-        (_openApiComponents openApi)
-          { _componentsSecuritySchemes =
-              _componentsSecuritySchemes (_openApiComponents openApi)
-                <> SecurityDefinitions (HM.singleton securityIdentifier securityScheme)
-          }
-    }
-
-addSecurityRequirement :: SecurityRequirement -> OpenApi -> OpenApi
-addSecurityRequirement securityRequirement = allOperations . security %~ (securityRequirement :)
-
-instance (HasOpenApi api, AuthMethods auths) => HasOpenApi (Servant.Auth.Auth auths a :> api) where
-  toOpenApi Proxy = addAuthMethods (authMethods (Proxy @auths)) $ toOpenApi $ Proxy @api
 
 instance HasOpenApi (RawM' a) where
   toOpenApi Proxy = toOpenApi $ Proxy @Raw
---
+++ server/source/library/Servant/Auth/OrphanInstances.hs
@@ -0,0 +1,94 @@
+{-# LANGUAGE GADTs #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+module Servant.Auth.OrphanInstances () where
+
+import Control.Lens
+import Data.HashMap.Strict.InsOrd qualified as HM
+import Data.Kind
+import Data.OpenApi
+import Data.Proxy
+import Data.Text qualified as T
+import Servant.API
+import Servant.Auth qualified
+import Servant.OpenApi
+
+instance (HasOpenApi api, AuthMethods auths) => HasOpenApi (Servant.Auth.Auth auths a :> api) where
+  toOpenApi Proxy = addAuthMethods (authMethods (Proxy @auths)) $ toOpenApi $ Proxy @api
+
+addAuthMethods :: AuthMethodList auths -> OpenApi -> OpenApi
+addAuthMethods = \case
+  MethodBasicAuth nextMethod ->
+    let
+      identifier :: T.Text = "BasicAuth"
+      addThisScheme =
+        addSecurityScheme identifier $
+          SecurityScheme
+            { _securitySchemeType = SecuritySchemeHttp HttpSchemeBasic
+            , _securitySchemeDescription = Just "Basic Authentication"
+            }
+      addThisRequirement = addSecurityRequirement $ SecurityRequirement $ HM.singleton identifier []
+     in
+      addAuthMethods nextMethod . addThisRequirement . addThisScheme
+  MethodJWT nextMethod ->
+    let
+      identifier :: T.Text = "JWT"
+      addThisScheme =
+        addSecurityScheme identifier $
+          SecurityScheme
+            { _securitySchemeType = SecuritySchemeHttp $ HttpSchemeBearer $ Just "JWT"
+            , _securitySchemeDescription = Just "Bearer Authentication"
+            }
+      addThisRequirement = addSecurityRequirement $ SecurityRequirement $ HM.singleton identifier []
+     in
+      addAuthMethods nextMethod . addThisRequirement . addThisScheme
+  MethodCookie nextMethod ->
+    let
+      identifier :: T.Text = "Cookie"
+      addThisScheme =
+        addSecurityScheme identifier $
+          SecurityScheme
+            { _securitySchemeType = SecuritySchemeHttp $ HttpSchemeBearer $ Just "JWT"
+            , _securitySchemeDescription = Just "Cookie Authentication"
+            }
+      addThisRequirement = addSecurityRequirement $ SecurityRequirement $ HM.singleton identifier []
+     in
+      addAuthMethods nextMethod . addThisRequirement . addThisScheme
+  MethodNone -> id
+
+type AuthMethodList :: [Type] -> Type
+data AuthMethodList xs :: Type where
+  MethodBasicAuth :: AuthMethodList auths -> AuthMethodList (Servant.Auth.BasicAuth ': auths)
+  MethodJWT :: AuthMethodList auths -> AuthMethodList (Servant.Auth.JWT ': auths)
+  MethodCookie :: AuthMethodList auths -> AuthMethodList (Servant.Auth.Cookie ': auths)
+  MethodNone :: AuthMethodList '[]
+
+type AuthMethods :: [Type] -> Constraint
+class AuthMethods auths where
+  authMethods :: Proxy auths -> AuthMethodList auths
+
+instance AuthMethods '[] where
+  authMethods Proxy = MethodNone
+
+instance AuthMethods auths => AuthMethods (Servant.Auth.BasicAuth ': auths) where
+  authMethods Proxy = MethodBasicAuth $ authMethods Proxy
+
+instance AuthMethods auths => AuthMethods (Servant.Auth.JWT ': auths) where
+  authMethods Proxy = MethodJWT $ authMethods Proxy
+
+instance AuthMethods auths => AuthMethods (Servant.Auth.Cookie ': auths) where
+  authMethods Proxy = MethodCookie $ authMethods Proxy
+
+addSecurityScheme :: T.Text -> SecurityScheme -> OpenApi -> OpenApi
+addSecurityScheme securityIdentifier securityScheme openApi =
+  openApi
+    { _openApiComponents =
+        (_openApiComponents openApi)
+          { _componentsSecuritySchemes =
+              _componentsSecuritySchemes (_openApiComponents openApi)
+                <> SecurityDefinitions (HM.singleton securityIdentifier securityScheme)
+          }
+    }
+
+addSecurityRequirement :: SecurityRequirement -> OpenApi -> OpenApi
+addSecurityRequirement securityRequirement = allOperations . security %~ (securityRequirement :)
