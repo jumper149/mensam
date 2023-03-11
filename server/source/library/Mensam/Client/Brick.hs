bdb27e4cd26680cad24b6eb9534b2ec5d70dc317
Felix Springer <felixspringer149@gmail.com>
2023-03-12 00:30:18 +0100

    Built working login with brick client.

--- server/source/library/Mensam/Client/Brick.hs
+++ server/source/library/Mensam/Client/Brick.hs
@@ -1,81 +1,118 @@
+{-# LANGUAGE MonoLocalBinds #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module Mensam.Client.Brick where
 
+import Mensam.Client.OrphanInstances
+import Mensam.Server.Route.Type qualified as Route
+import Mensam.Server.Route.User.Type qualified as Route.User
+
 import Brick
 import Brick.Forms
+import Control.Monad.IO.Class
+import Data.Kind
+import Data.SOP
 import Data.Text qualified as T
 import Graphics.Vty
 import Lens.Micro.Platform
+import Network.HTTP.Client qualified as Network
+import Servant
+import Servant.Client
 import Servant.RawM.Client ()
-import Data.Kind
+
+routes :: Route.Routes (AsClientT ClientM)
+routes = client $ Proxy @(NamedRoutes Route.Routes)
 
 type ClientName :: Type
 data ClientName
-    = ClientNameLoginUsername
-    | ClientNameLoginPassword
+  = ClientNameLoginUsername
+  | ClientNameLoginPassword
   deriving stock (Eq, Ord, Show)
 
-type LoginState :: Type
-data LoginState = MkLoginState
-    { _loginStateUsername :: T.Text
-    , _loginStatePassword :: T.Text
-    }
-makeLenses ''LoginState
+type LoginInfo :: Type
+data LoginInfo = MkLoginInfo
+  { _loginInfoUsername :: T.Text
+  , _loginInfoPassword :: T.Text
+  }
+makeLenses ''LoginInfo
 
-loginFormInitial :: Form LoginState e ClientName
+loginFormInitial :: Form LoginInfo e ClientName
 loginFormInitial =
   newForm
-    [ (str "Username: " <+>) @@= editTextField loginStateUsername ClientNameLoginUsername (Just 1)
-    , (str "Password: " <+>) @@= editTextField loginStatePassword ClientNameLoginPassword (Just 1)
+    [ (str "Username: " <+>) @@= editTextField loginInfoUsername ClientNameLoginUsername (Just 1)
+    , (str "Password: " <+>) @@= editTextField loginInfoPassword ClientNameLoginPassword (Just 1)
     ]
-    MkLoginState
-      { _loginStateUsername = ""
-      , _loginStatePassword = ""
+    MkLoginInfo
+      { _loginInfoUsername = ""
+      , _loginInfoPassword = ""
       }
 
 type ClientState :: Type -> Type
-data ClientState e = MkClientState
-  { _clientStateLogin :: Form LoginState e ClientName
-  , _clientStateJWT :: Maybe T.Text
-  }
+data ClientState e
+  = ClientStateLogin {_clientStateLoginForm :: Form LoginInfo e ClientName}
+  | ClientStateLoggedIn {_clientStateJwt :: T.Text}
 makeLenses ''ClientState
 
 type ClientEvent :: Type
 data ClientEvent
-    = ClientEventLoginRequire
-    | ClientEventLoginSend
+  = ClientEventLoginRequire
+  | ClientEventLoginSend
   deriving stock (Eq, Ord, Show)
 
 main :: IO ()
 main = do
+  httpManager <- Network.newManager Network.defaultManagerSettings
+  let baseUrl =
+        BaseUrl
+          { baseUrlScheme = Http
+          , baseUrlHost = "localhost"
+          , baseUrlPort = 8177
+          , baseUrlPath = ""
+          }
+  let clientEnv = mkClientEnv httpManager baseUrl
   let
     app :: App (ClientState ClientEvent) ClientEvent ClientName
-    app = App
-      { appDraw = \case
-          MkClientState { _clientStateJWT = Nothing, _clientStateLogin = loginForm } -> [renderForm loginForm]
-          MkClientState { _clientStateJWT = Just jwt } -> [txt jwt]
-      , appChooseCursor = \_ cursors ->
-          case cursors of
-            [] -> Nothing
-            x : _ -> pure x
-      , appHandleEvent = \case
-        --AppEvent ClientEventLoginRequire -> halt
-        VtyEvent (EvKey KEnter []) -> do
-            clientState <- get
-            case _clientStateJWT clientState of
-              Nothing -> undefined
-              Just _ -> pure ()
-            pure ()
-        VtyEvent (EvKey KEsc []) -> halt
-        event -> zoom clientStateLogin $ handleFormEvent event
-      , appStartEvent = pure ()
-      , appAttrMap = \_ -> attrMap defAttr []
-      }
+    app =
+      App
+        { appDraw = \case
+            ClientStateLogin form -> [renderForm form]
+            _ -> [txt "Hello"]
+        , appChooseCursor = \_ cursors ->
+            case cursors of
+              [] -> Nothing
+              x : _ -> pure x
+        , appHandleEvent = \case
+            VtyEvent (EvKey KEsc []) -> halt
+            -- AppEvent ClientEventLoginRequire -> halt
+            event -> do
+              clientState <- get
+              case clientState of
+                ClientStateLogin form ->
+                  case event of
+                    VtyEvent (EvKey KEnter []) ->
+                      case formState form of
+                        loginInfo -> do
+                          result <-
+                            liftIO $
+                              flip runClientM clientEnv $
+                                routes // Route.routeUser // Route.User.routeLogin $
+                                  DataBasicAuth
+                                    MkCredentials
+                                      { credentialsUsername = loginInfo ^. loginInfoUsername
+                                      , credentialsPassword = loginInfo ^. loginInfoPassword
+                                      }
+                          case result of
+                            Right (Z (I (WithStatus @200 (Route.User.MkResponseLogin jwt)))) ->
+                              put $ ClientStateLoggedIn jwt
+                            _ -> pure ()
+
+                          pure ()
+                    _ -> zoom clientStateLoginForm $ handleFormEvent event
+                _ -> pure ()
+        , appStartEvent = pure ()
+        , appAttrMap = \_ -> attrMap defAttr []
+        }
     initialState :: ClientState ClientEvent
-    initialState = MkClientState
-      { _clientStateLogin = loginFormInitial
-      , _clientStateJWT = Nothing
-      }
+    initialState = ClientStateLogin loginFormInitial
   _finalState <- defaultMain app initialState
   pure ()
