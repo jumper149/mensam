e015a6ccbc01cb520a8ec6bf04b1879b86e97cf5
Felix Springer <felixspringer149@gmail.com>
2023-03-13 11:58:30 +0100

    Follow the Redoc example closely.

--- server/source/library/Mensam/Server/Server/Route/OpenApi.hs
+++ server/source/library/Mensam/Server/Server/Route/OpenApi.hs
@@ -40,25 +40,31 @@ render :: Monad m => m Blaze.Html
 render =
   pure $
     Blaze.docTypeHtml $ do
-      Blaze.head $ Blaze.title $ Blaze.toMarkup @T.Text "Mensam API"
-      -- TODO: Check if this is actually required.
-      --     <!-- needed for adaptive design -->
-      --     <meta charset="utf-8"/>
-      --     <meta name="viewport" content="width=device-width, initial-scale=1">
-      --     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700|Roboto:300,400,700" rel="stylesheet">
-      --
-      --     <!--
-      --     Redoc doesn't change outer page styles
-      --     -->
-      --     <style>
-      --       body {
-      --         margin: 0;
-      --         padding: 0;
-      --       }
-      --     </style>
+      Blaze.head $ do
+        Blaze.title $ Blaze.toMarkup @T.Text "Mensam API"
+
+        --  needed for adaptive design
+        Blaze.meta
+          Blaze.! Blaze.Attributes.charset "utf-8"
+        Blaze.meta
+          Blaze.! Blaze.Attributes.name "viewport"
+          Blaze.! Blaze.Attributes.content "width=device-width, initial-scale=1"
+        -- TODO: Avoiding dependency for now. We should host fonts ourselves if we want this.
+        -- Blaze.link
+        --   Blaze.! Blaze.Attributes.href "https://fonts.googleapis.com/css?family=Montserrat:300,400,700|Roboto:300,400,700"
+        --   Blaze.! Blaze.Attributes.rel "stylesheet"
+
+        -- Redoc doesn't change outer page styles
+        Blaze.style
+          "body {\n\
+          \  margin: 0;\n\
+          \  padding: 0;\n\
+          \}"
+
       Blaze.body $ do
         let
           redoc = Blaze.Internal.customParent $ Blaze.textTag "redoc"
           specUrl = Blaze.customAttribute "spec-url"
         redoc Blaze.! specUrl "./openapi/json" $ ""
+        -- TODO: We should host Redoc ourselves.
         Blaze.script Blaze.! Blaze.Attributes.src "https://cdn.redoc.ly/redoc/latest/bundles/redoc.standalone.js" $ ""
