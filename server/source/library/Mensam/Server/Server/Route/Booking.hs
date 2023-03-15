8c6d483c13c35f8b7212e761a7d328ec50747038
Felix Springer <felixspringer149@gmail.com>
2023-03-15 23:41:21 +0100

    Use wrapper to handle parsing errors for request bodies.

--- server/source/library/Mensam/Server/Server/Route/Booking.hs
+++ server/source/library/Mensam/Server/Server/Route/Booking.hs
@@ -42,29 +42,27 @@ createSpace ::
   Either String RequestSpaceCreate ->
   m (Union responses)
 createSpace authUser eitherRequest =
-  handleAuth authUser $ \user -> do
-    request <- case eitherRequest of
-      Left _err -> undefined
-      Right request -> pure request
-    logDebug $ "Received request to create space: " <> T.pack (show request)
-    seldaResult <- runSeldaTransactionT $ do
-      spaceCreate (requestSpaceCreateName request) (requestSpaceCreateVisible request)
-      spaceIdentifier <-
-        spaceLookupId (requestSpaceCreateName request) >>= \case
-          Just identifier -> pure identifier
-          Nothing -> do
-            let msg :: T.Text = "No matching space even though it was just created."
-            lift $ logError msg
-            throwM $ Selda.SqlError $ show msg
-      spaceUserAdd spaceIdentifier (userId user) True
-    case seldaResult of
-      SeldaFailure _err -> do
-        -- TODO: Here we can theoretically return a more accurate error
-        logWarn "Failed to create space."
-        respond $ WithStatus @500 ()
-      SeldaSuccess () -> do
-        logInfo "Created space."
-        respond $ WithStatus @201 ()
+  handleAuth authUser $ \user ->
+    handleBadRequestBody eitherRequest $ \request -> do
+      logDebug $ "Received request to create space: " <> T.pack (show request)
+      seldaResult <- runSeldaTransactionT $ do
+        spaceCreate (requestSpaceCreateName request) (requestSpaceCreateVisible request)
+        spaceIdentifier <-
+          spaceLookupId (requestSpaceCreateName request) >>= \case
+            Just identifier -> pure identifier
+            Nothing -> do
+              let msg :: T.Text = "No matching space even though it was just created."
+              lift $ logError msg
+              throwM $ Selda.SqlError $ show msg
+        spaceUserAdd spaceIdentifier (userId user) True
+      case seldaResult of
+        SeldaFailure _err -> do
+          -- TODO: Here we can theoretically return a more accurate error
+          logWarn "Failed to create space."
+          respond $ WithStatus @500 ()
+        SeldaSuccess () -> do
+          logInfo "Created space."
+          respond $ WithStatus @201 ()
 
 listSpaces ::
   ( MonadIO m
@@ -79,21 +77,19 @@ listSpaces ::
   Either String RequestSpaceList ->
   m (Union responses)
 listSpaces authUser eitherRequest =
-  handleAuth authUser $ \user -> do
-    request <- case eitherRequest of
-      Left _err -> undefined
-      Right request -> pure request
-    logDebug $ "Received request to list spaces: " <> T.pack (show request)
-    seldaResult <- runSeldaTransactionT $ do
-      spaceList (userId user) (requestSpaceListOrder request)
-    case seldaResult of
-      SeldaFailure _err -> do
-        -- TODO: Here we can theoretically return a more accurate error
-        logWarn "Failed to list spaces."
-        respond $ WithStatus @500 ()
-      SeldaSuccess spaces -> do
-        logInfo "Listed spaces."
-        respond $ WithStatus @200 MkResponseSpaceList {responseSpaceListSpaces = spaces}
+  handleAuth authUser $ \user ->
+    handleBadRequestBody eitherRequest $ \request -> do
+      logDebug $ "Received request to list spaces: " <> T.pack (show request)
+      seldaResult <- runSeldaTransactionT $ do
+        spaceList (userId user) (requestSpaceListOrder request)
+      case seldaResult of
+        SeldaFailure _err -> do
+          -- TODO: Here we can theoretically return a more accurate error
+          logWarn "Failed to list spaces."
+          respond $ WithStatus @500 ()
+        SeldaSuccess spaces -> do
+          logInfo "Listed spaces."
+          respond $ WithStatus @200 MkResponseSpaceList {responseSpaceListSpaces = spaces}
 
 createDesk ::
   ( MonadIO m
@@ -108,35 +104,33 @@ createDesk ::
   Either String RequestDeskCreate ->
   m (Union responses)
 createDesk authUser eitherRequest =
-  handleAuth authUser $ \user -> do
-    request <- case eitherRequest of
-      Left _err -> undefined
-      Right request -> pure request
-    logDebug $ "Received request to create desk: " <> T.pack (show request)
-    seldaResult <- runSeldaTransactionT $ do
-      spaceIdentifier <-
-        case requestDeskCreateSpace request of
-          Identifier spaceId -> pure spaceId
-          Name name ->
-            spaceLookupId name >>= \case
-              Just identifier -> pure identifier
-              Nothing -> do
-                let msg :: T.Text = "No matching space."
-                lift $ logWarn msg
-                throwM $ Selda.SqlError $ show msg
-      permission <- spaceUserLookup spaceIdentifier (userId user)
-      case permission of
-        Nothing -> error "No permission."
-        Just False -> error "No admin permission."
-        Just True -> deskCreate (requestDeskCreateName request) spaceIdentifier
-    case seldaResult of
-      SeldaFailure _err -> do
-        -- TODO: Here we can theoretically return a more accurate error
-        logWarn "Failed to create desk."
-        respond $ WithStatus @500 ()
-      SeldaSuccess () -> do
-        logInfo "Created desk."
-        respond $ WithStatus @201 ()
+  handleAuth authUser $ \user ->
+    handleBadRequestBody eitherRequest $ \request -> do
+      logDebug $ "Received request to create desk: " <> T.pack (show request)
+      seldaResult <- runSeldaTransactionT $ do
+        spaceIdentifier <-
+          case requestDeskCreateSpace request of
+            Identifier spaceId -> pure spaceId
+            Name name ->
+              spaceLookupId name >>= \case
+                Just identifier -> pure identifier
+                Nothing -> do
+                  let msg :: T.Text = "No matching space."
+                  lift $ logWarn msg
+                  throwM $ Selda.SqlError $ show msg
+        permission <- spaceUserLookup spaceIdentifier (userId user)
+        case permission of
+          Nothing -> error "No permission."
+          Just False -> error "No admin permission."
+          Just True -> deskCreate (requestDeskCreateName request) spaceIdentifier
+      case seldaResult of
+        SeldaFailure _err -> do
+          -- TODO: Here we can theoretically return a more accurate error
+          logWarn "Failed to create desk."
+          respond $ WithStatus @500 ()
+        SeldaSuccess () -> do
+          logInfo "Created desk."
+          respond $ WithStatus @201 ()
 
 listDesks ::
   ( MonadIO m
@@ -151,28 +145,26 @@ listDesks ::
   Either String RequestDeskList ->
   m (Union responses)
 listDesks authUser eitherRequest =
-  handleAuth authUser $ \user -> do
-    request <- case eitherRequest of
-      Left _err -> undefined
-      Right request -> pure request
-    logDebug $ "Received request to list desks: " <> T.pack (show request)
-    seldaResult <- runSeldaTransactionT $ do
-      spaceIdentifier <-
-        case requestDeskListSpace request of
-          Name spaceName ->
-            spaceLookupId spaceName >>= \case
-              Nothing -> undefined
-              Just spaceId -> pure spaceId
-          Identifier spaceId -> pure spaceId
-      deskList spaceIdentifier (userId user)
-    case seldaResult of
-      SeldaFailure _err -> do
-        -- TODO: Here we can theoretically return a more accurate error
-        logWarn "Failed to list desks."
-        respond $ WithStatus @500 ()
-      SeldaSuccess desks -> do
-        logInfo "Listed desks."
-        respond $ WithStatus @200 MkResponseDeskList {responseDeskListDesks = desks}
+  handleAuth authUser $ \user ->
+    handleBadRequestBody eitherRequest $ \request -> do
+      logDebug $ "Received request to list desks: " <> T.pack (show request)
+      seldaResult <- runSeldaTransactionT $ do
+        spaceIdentifier <-
+          case requestDeskListSpace request of
+            Name spaceName ->
+              spaceLookupId spaceName >>= \case
+                Nothing -> undefined
+                Just spaceId -> pure spaceId
+            Identifier spaceId -> pure spaceId
+        deskList spaceIdentifier (userId user)
+      case seldaResult of
+        SeldaFailure _err -> do
+          -- TODO: Here we can theoretically return a more accurate error
+          logWarn "Failed to list desks."
+          respond $ WithStatus @500 ()
+        SeldaSuccess desks -> do
+          logInfo "Listed desks."
+          respond $ WithStatus @200 MkResponseDeskList {responseDeskListDesks = desks}
 
 createReservation ::
   ( MonadIO m
@@ -187,25 +179,36 @@ createReservation ::
   Either String RequestReservationCreate ->
   m (Union responses)
 createReservation authUser eitherRequest = do
-  handleAuth authUser $ \user -> do
-    request <- case eitherRequest of
-      Left _err -> undefined
-      Right request -> pure request
-    logDebug $ "Received request to create reservation: " <> T.pack (show request)
-    seldaResult <- runSeldaTransactionT $ do
-      deskIdentifier <-
-        case requestReservationCreateDesk request of
-          Name deskName ->
-            deskLookupId deskName >>= \case
-              Nothing -> undefined
-              Just deskId -> pure deskId
-          Identifier deskId -> pure deskId
-      reservationCreate deskIdentifier (userId user) (requestReservationCreateTimeBegin request) (requestReservationCreateTimeEnd request)
-    case seldaResult of
-      SeldaFailure _err -> do
-        -- TODO: Here we can theoretically return a more accurate error
-        logWarn "Failed to create desk."
-        respond $ WithStatus @500 ()
-      SeldaSuccess () -> do
-        logInfo "Created desk."
-        respond $ WithStatus @201 $ MkResponseReservationCreate ()
+  handleAuth authUser $ \user ->
+    handleBadRequestBody eitherRequest $ \request -> do
+      logDebug $ "Received request to create reservation: " <> T.pack (show request)
+      seldaResult <- runSeldaTransactionT $ do
+        deskIdentifier <-
+          case requestReservationCreateDesk request of
+            Name deskName ->
+              deskLookupId deskName >>= \case
+                Nothing -> undefined
+                Just deskId -> pure deskId
+            Identifier deskId -> pure deskId
+        reservationCreate deskIdentifier (userId user) (requestReservationCreateTimeBegin request) (requestReservationCreateTimeEnd request)
+      case seldaResult of
+        SeldaFailure _err -> do
+          -- TODO: Here we can theoretically return a more accurate error
+          logWarn "Failed to create desk."
+          respond $ WithStatus @500 ()
+        SeldaSuccess () -> do
+          logInfo "Created desk."
+          respond $ WithStatus @201 $ MkResponseReservationCreate ()
+
+handleBadRequestBody ::
+  ( MonadLogger m
+  , IsMember (WithStatus 400 ()) responses
+  ) =>
+  Either String a ->
+  (a -> m (Union responses)) ->
+  m (Union responses)
+handleBadRequestBody parsedRequestBody handler' =
+  -- TODO: Rename arguments `handler'` to `handler`
+  case parsedRequestBody of
+    Right a -> handler' a
+    Left _err -> respond $ WithStatus @400 () -- TODO: Respond with error.
