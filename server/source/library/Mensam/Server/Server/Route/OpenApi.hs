ad6a723f9f0655e1f2214782cb18408b15d6208b
Felix Springer <felixspringer149@gmail.com>
2023-03-24 01:01:48 +0100

    Use servant link for OpenApi definition.

--- server/source/library/Mensam/Server/Server/Route/OpenApi.hs
+++ server/source/library/Mensam/Server/Server/Route/OpenApi.hs
@@ -1,9 +1,15 @@
 module Mensam.Server.Server.Route.OpenApi where
 
+import Mensam.API.Route qualified as Route
+import Mensam.API.Route.Api qualified as Route.Api
+import Mensam.API.Route.Api.OpenApi qualified as Route.Api.OpenApi
 import Mensam.API.Route.OpenApi
+import Mensam.API.Route.OpenApi qualified as Route.OpenApi
 import Mensam.Server.Application.Configured.Class
 
+import Data.List qualified as L
 import Data.Text qualified as T
+import Servant.Links
 import Servant.Server.Generic
 import Text.Blaze.Html qualified as Blaze
 import Text.Blaze.Html5 qualified as Blaze
@@ -47,7 +53,20 @@ render =
         let
           redoc = Blaze.Internal.customParent $ Blaze.textTag "redoc"
           specUrl = Blaze.customAttribute "spec-url"
-        -- TODO: Use servant link instead of this hardcoded link.
-        redoc Blaze.! specUrl "./api/openapi" $ ""
+          linkCurrent = Route.OpenApi.routeRender . Route.routeOpenApi $ allFieldLinks
+          linkOpenApiJson = Route.Api.OpenApi.routeJson . Route.Api.routeOpenApi . Route.routeApi $ allFieldLinks
+        redoc Blaze.! specUrl (Blaze.stringValue $ relativePath linkCurrent linkOpenApiJson) $ ""
         -- TODO: We should host Redoc ourselves.
         Blaze.script Blaze.! Blaze.Attributes.src "https://cdn.redoc.ly/redoc/latest/bundles/redoc.standalone.js" $ ""
+
+-- | Assuming that links don't have trailing slashes.
+relativePath :: Link -> Link -> String
+relativePath origin destination = goBack ++ "/" ++ goForward
+ where
+  goBack :: String
+  goBack = case linkSegments origin of
+    [] -> "."
+    [_] -> "."
+    _ : baseSegments -> L.intercalate "/" (".." <$ baseSegments)
+  goForward :: String
+  goForward = show $ linkURI destination
