17bc82a42f3e369ef5d19d274f7a3aded200e0b6
Felix Springer <felixspringer149@gmail.com>
2023-03-24 01:01:51 +0100

    Use relativ link in the served OpenApi.

--- server/source/library/Mensam/Server/Server/Route/Api/OpenApi.hs
+++ server/source/library/Mensam/Server/Server/Route/Api/OpenApi.hs
@@ -1,12 +1,16 @@
 module Mensam.Server.Server.Route.Api.OpenApi where
 
+import Mensam.API.Route.Api qualified as Route.Api
 import Mensam.API.Route.Api.OpenApi
 import Mensam.Server.Application.Configured.Class
 import Mensam.Server.Configuration
 import Mensam.Server.OpenApi qualified
 
 import Control.Lens
+import Data.List qualified as L
 import Data.OpenApi
+import Data.Text qualified as T
+import Servant.Links
 import Servant.Server.Generic
 
 handler ::
@@ -31,11 +35,21 @@ specification = do
      where
       relativeServer =
         Server
-          { _serverUrl = ""
+          { _serverUrl = T.pack relativePath
           , _serverDescription = Nothing
           , _serverVariables = mempty
           }
+      linkCurrent = routeJson . Route.Api.routeOpenApi $ allFieldLinks
+      relativePath = relativePathGoBack linkCurrent
   pure $
     Mensam.Server.OpenApi.openapi
       & addVersion
       & addServer
+
+-- | Assuming that the origin link doesn't have a trailing slash.
+relativePathGoBack :: Servant.Links.Link -> String
+relativePathGoBack origin =
+  case linkSegments origin of
+    [] -> ""
+    [_] -> ""
+    _ : baseSegments -> L.intercalate "/" (".." <$ baseSegments)
