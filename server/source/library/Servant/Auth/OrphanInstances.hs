7ad040926cc388456292a33073dc44bd37a61201
Felix Springer <felixspringer149@gmail.com>
2023-03-31 00:01:17 +0200

    Write `HasOpenApi` instance for servant-auth without GADT.

--- server/source/library/Servant/Auth/OrphanInstances.hs
+++ server/source/library/Servant/Auth/OrphanInstances.hs
@@ -4,7 +4,6 @@ module Servant.Auth.OrphanInstances () where
 
 import Control.Lens
 import Data.HashMap.Strict.InsOrd qualified as HM
-import Data.Kind
 import Data.OpenApi
 import Data.Proxy
 import Data.Text qualified as T
@@ -12,71 +11,41 @@ import Servant.API
 import Servant.Auth qualified
 import Servant.OpenApi
 
-instance (HasOpenApi api, AuthMethods auths) => HasOpenApi (Servant.Auth.Auth auths a :> api) where
-  toOpenApi Proxy = addAuthMethods (authMethods (Proxy @auths)) $ toOpenApi $ Proxy @api
+instance (HasOpenApi api) => HasOpenApi (Servant.Auth.Auth '[] a :> api) where
+  toOpenApi Proxy = toOpenApi $ Proxy @api
 
-addAuthMethods :: AuthMethodList auths -> OpenApi -> OpenApi
-addAuthMethods = \case
-  MethodBasicAuth nextMethod ->
-    let
-      identifier :: T.Text = "BasicAuth"
-      securityScheme =
-        SecurityScheme
-          { _securitySchemeType = SecuritySchemeHttp HttpSchemeBasic
-          , _securitySchemeDescription = Just "Basic Authentication"
-          }
-     in
-      addAuthMethods nextMethod
-        . addSecurityRequirement identifier
-        . addSecurityScheme identifier securityScheme
-  MethodJWT nextMethod ->
-    let
-      identifier :: T.Text = "JWT"
-      securityScheme =
-        SecurityScheme
-          { _securitySchemeType = SecuritySchemeHttp $ HttpSchemeBearer $ Just "JWT"
-          , _securitySchemeDescription = Just "Bearer Authentication"
-          }
-     in
-      addAuthMethods nextMethod
-        . addSecurityRequirement identifier
-        . addSecurityScheme identifier securityScheme
-  MethodCookie nextMethod ->
-    let
-      identifier :: T.Text = "Cookie"
-      securityScheme =
-        SecurityScheme
-          { _securitySchemeType = SecuritySchemeHttp $ HttpSchemeBearer $ Just "JWT"
-          , _securitySchemeDescription = Just "Cookie Authentication"
-          }
-     in
-      addAuthMethods nextMethod
-        . addSecurityRequirement identifier
-        . addSecurityScheme identifier securityScheme
-  MethodNone -> id
+instance (HasOpenApi (Servant.Auth.Auth auths a :> api)) => HasOpenApi (Servant.Auth.Auth (Servant.Auth.BasicAuth : auths) a :> api) where
+  toOpenApi Proxy = addSecurity $ toOpenApi $ Proxy @(Servant.Auth.Auth auths a :> api)
+   where
+    addSecurity = addSecurityRequirement identifier . addSecurityScheme identifier securityScheme
+    identifier :: T.Text = "BasicAuth"
+    securityScheme =
+      SecurityScheme
+        { _securitySchemeType = SecuritySchemeHttp HttpSchemeBasic
+        , _securitySchemeDescription = Just "Basic Authentication"
+        }
 
-type AuthMethodList :: [Type] -> Type
-data AuthMethodList xs :: Type where
-  MethodBasicAuth :: AuthMethodList auths -> AuthMethodList (Servant.Auth.BasicAuth ': auths)
-  MethodJWT :: AuthMethodList auths -> AuthMethodList (Servant.Auth.JWT ': auths)
-  MethodCookie :: AuthMethodList auths -> AuthMethodList (Servant.Auth.Cookie ': auths)
-  MethodNone :: AuthMethodList '[]
+instance (HasOpenApi (Servant.Auth.Auth auths a :> api)) => HasOpenApi (Servant.Auth.Auth (Servant.Auth.JWT : auths) a :> api) where
+  toOpenApi Proxy = addSecurity $ toOpenApi $ Proxy @(Servant.Auth.Auth auths a :> api)
+   where
+    addSecurity = addSecurityRequirement identifier . addSecurityScheme identifier securityScheme
+    identifier :: T.Text = "JWT"
+    securityScheme =
+      SecurityScheme
+        { _securitySchemeType = SecuritySchemeHttp $ HttpSchemeBearer $ Just "JWT"
+        , _securitySchemeDescription = Just "Bearer Authentication"
+        }
 
-type AuthMethods :: [Type] -> Constraint
-class AuthMethods auths where
-  authMethods :: Proxy auths -> AuthMethodList auths
-
-instance AuthMethods '[] where
-  authMethods Proxy = MethodNone
-
-instance AuthMethods auths => AuthMethods (Servant.Auth.BasicAuth ': auths) where
-  authMethods Proxy = MethodBasicAuth $ authMethods Proxy
-
-instance AuthMethods auths => AuthMethods (Servant.Auth.JWT ': auths) where
-  authMethods Proxy = MethodJWT $ authMethods Proxy
-
-instance AuthMethods auths => AuthMethods (Servant.Auth.Cookie ': auths) where
-  authMethods Proxy = MethodCookie $ authMethods Proxy
+instance (HasOpenApi (Servant.Auth.Auth auths a :> api)) => HasOpenApi (Servant.Auth.Auth (Servant.Auth.Cookie : auths) a :> api) where
+  toOpenApi Proxy = addSecurity $ toOpenApi $ Proxy @(Servant.Auth.Auth auths a :> api)
+   where
+    addSecurity = addSecurityRequirement identifier . addSecurityScheme identifier securityScheme
+    identifier :: T.Text = "Cookie"
+    securityScheme =
+      SecurityScheme
+        { _securitySchemeType = SecuritySchemeHttp $ HttpSchemeBearer $ Just "JWT"
+        , _securitySchemeDescription = Just "Cookie Authentication"
+        }
 
 addSecurityScheme :: T.Text -> SecurityScheme -> OpenApi -> OpenApi
 addSecurityScheme securityIdentifier securityScheme openApi =
