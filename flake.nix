a8b833d00db3fa022b1861153ed17320955ad2c6
Felix Springer <felixspringer149@gmail.com>
2023-03-30 18:21:50 +0200

    Use Asciidoctor to write Readme.

--- flake.nix
+++ flake.nix
@@ -34,6 +34,7 @@
       with import nixpkgs { system = "x86_64-linux"; overlays = [ self.subflakes.setup.overlays.default ]; };
       let
         additionalBuildInputs = [
+          pkgs.asciidoctor
           pkgs.haskell.packages.ghc926.nix-tree
           pkgs.rnix-lsp
           pkgs.sqlite
