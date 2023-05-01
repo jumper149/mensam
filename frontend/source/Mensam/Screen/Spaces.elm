8513247787fe6cc0076020f9f05d4526e4d90e9a
Felix Springer <felixspringer149@gmail.com>
2023-05-01 23:52:50 +0200

    Add button to create new space.

--- frontend/source/Mensam/Screen/Spaces.elm
+++ frontend/source/Mensam/Screen/Spaces.elm
@@ -7,6 +7,7 @@ import Element.Font
 import Html.Attributes
 import Mensam.Api.Login
 import Mensam.Api.SpaceList
+import Mensam.Color
 import Mensam.Error
 import Mensam.Font
 import Mensam.Jwt
@@ -15,119 +16,158 @@ import Mensam.Jwt
 type alias Model =
     { spaces : List { id : Int, name : String }
     , selected : Maybe Int
+    , create : Maybe ()
     }
 
 
 init : Model
 init =
-    { spaces = [], selected = Nothing }
+    { spaces = []
+    , selected = Nothing
+    , create = Nothing
+    }
 
 
 element : Model -> Element.Element Message
 element model =
-    Element.el
+    Element.column
         [ Element.width Element.fill
-        , Element.height Element.fill
-        , Element.padding 10
-        , Element.Background.color (Element.rgba 0 0 0 0.1)
-        , Element.Font.size 16
-        , Element.Font.family [ Mensam.Font.condensed ]
         ]
-    <|
-        Element.indexedTable
-            [ Element.Events.onMouseLeave <| MessagePure <| SetSelected Nothing
+        [ Element.el
+            [ Element.width Element.fill
+            , Element.height <| Element.px 70
+            , Element.padding 10
             ]
-            { data = model.spaces
-            , columns =
-                let
-                    cell =
-                        Element.el
-                            [ Element.height <| Element.px 40
-                            , Element.padding 10
-                            ]
-                in
-                [ { header =
-                        Element.el
-                            [ Element.Background.color (Element.rgba 0 0 0 0.3)
-                            ]
-                        <|
-                            cell <|
-                                Element.el
-                                    []
-                                <|
-                                    Element.text "ID"
-                  , width = Element.px 100
-                  , view =
-                        \n x ->
-                            Element.el
-                                [ Element.Events.onMouseEnter <| MessagePure <| SetSelected <| Just n
-                                , Element.Events.onClick <| MessageEffect <| ChooseSpace { id = x.id }
-                                , Element.htmlAttribute <| Html.Attributes.style "cursor" "pointer"
-                                , let
-                                    alpha =
-                                        case model.selected of
-                                            Nothing ->
-                                                0.2
-
-                                            Just m ->
-                                                if m == n then
-                                                    0.4
-
-                                                else
-                                                    0.2
-                                  in
-                                  Element.Background.color (Element.rgba 0 0 0 alpha)
-                                ]
-                            <|
-                                cell <|
-                                    Element.el
-                                        [ Element.width <| Element.maximum 100 <| Element.fill ]
-                                    <|
-                                        Element.text <|
-                                            String.fromInt x.id
-                  }
-                , { header =
-                        Element.el
-                            [ Element.Background.color (Element.rgba 0 0 0 0.3)
-                            ]
-                        <|
-                            cell <|
-                                Element.el
-                                    []
-                                <|
-                                    Element.text "Name"
-                  , width = Element.fill
-                  , view =
-                        \n x ->
-                            Element.el
-                                [ Element.Events.onMouseEnter <| MessagePure <| SetSelected <| Just n
-                                , Element.Events.onClick <| MessageEffect <| ChooseSpace { id = x.id }
-                                , Element.htmlAttribute <| Html.Attributes.style "cursor" "pointer"
-                                , let
-                                    alpha =
-                                        case model.selected of
-                                            Nothing ->
-                                                0.2
-
-                                            Just m ->
-                                                if m == n then
-                                                    0.4
-
-                                                else
-                                                    0.2
-                                  in
-                                  Element.Background.color (Element.rgba 0 0 0 alpha)
-                                ]
-                            <|
-                                cell <|
-                                    Element.el
-                                        [ Element.width <| Element.maximum 100 <| Element.fill ]
-                                    <|
-                                        Element.text <|
-                                            x.name
-                  }
+          <|
+            Element.row
+                [ Element.width Element.fill
+                , Element.spacing 30
                 ]
-            }
+                [ Element.el
+                    [ Element.alignRight
+                    , Element.padding 10
+                    , Element.Background.color Mensam.Color.bright.yellow
+                    , Element.Font.color Mensam.Color.dark.black
+                    , Element.htmlAttribute <| Html.Attributes.style "cursor" "pointer"
+                    , Element.htmlAttribute <| Html.Attributes.style "user-select" "none"
+                    , Element.mouseOver [ Element.Background.color Mensam.Color.bright.green ]
+                    , Element.Events.onClick <| MessagePure OpenDialogToCreate
+                    ]
+                  <|
+                    Element.el
+                        [ Element.centerX
+                        , Element.centerY
+                        , Element.Font.family [ Mensam.Font.condensed ]
+                        , Element.Font.size 17
+                        , Element.htmlAttribute <| Html.Attributes.style "text-transform" "uppercase"
+                        ]
+                    <|
+                        Element.text "Create new Space"
+                ]
+        , Element.el
+            [ Element.width Element.fill
+            , Element.height Element.fill
+            , Element.padding 10
+            , Element.Background.color (Element.rgba 0 0 0 0.1)
+            , Element.Font.size 16
+            , Element.Font.family [ Mensam.Font.condensed ]
+            ]
+          <|
+            Element.indexedTable
+                [ Element.Events.onMouseLeave <| MessagePure <| SetSelected Nothing
+                ]
+                { data = model.spaces
+                , columns =
+                    let
+                        cell =
+                            Element.el
+                                [ Element.height <| Element.px 40
+                                , Element.padding 10
+                                ]
+                    in
+                    [ { header =
+                            Element.el
+                                [ Element.Background.color (Element.rgba 0 0 0 0.3)
+                                ]
+                            <|
+                                cell <|
+                                    Element.el
+                                        []
+                                    <|
+                                        Element.text "ID"
+                      , width = Element.px 100
+                      , view =
+                            \n x ->
+                                Element.el
+                                    [ Element.Events.onMouseEnter <| MessagePure <| SetSelected <| Just n
+                                    , Element.Events.onClick <| MessageEffect <| ChooseSpace { id = x.id }
+                                    , Element.htmlAttribute <| Html.Attributes.style "cursor" "pointer"
+                                    , let
+                                        alpha =
+                                            case model.selected of
+                                                Nothing ->
+                                                    0.2
+
+                                                Just m ->
+                                                    if m == n then
+                                                        0.4
+
+                                                    else
+                                                        0.2
+                                      in
+                                      Element.Background.color (Element.rgba 0 0 0 alpha)
+                                    ]
+                                <|
+                                    cell <|
+                                        Element.el
+                                            [ Element.width <| Element.maximum 100 <| Element.fill ]
+                                        <|
+                                            Element.text <|
+                                                String.fromInt x.id
+                      }
+                    , { header =
+                            Element.el
+                                [ Element.Background.color (Element.rgba 0 0 0 0.3)
+                                ]
+                            <|
+                                cell <|
+                                    Element.el
+                                        []
+                                    <|
+                                        Element.text "Name"
+                      , width = Element.fill
+                      , view =
+                            \n x ->
+                                Element.el
+                                    [ Element.Events.onMouseEnter <| MessagePure <| SetSelected <| Just n
+                                    , Element.Events.onClick <| MessageEffect <| ChooseSpace { id = x.id }
+                                    , Element.htmlAttribute <| Html.Attributes.style "cursor" "pointer"
+                                    , let
+                                        alpha =
+                                            case model.selected of
+                                                Nothing ->
+                                                    0.2
+
+                                                Just m ->
+                                                    if m == n then
+                                                        0.4
+
+                                                    else
+                                                        0.2
+                                      in
+                                      Element.Background.color (Element.rgba 0 0 0 alpha)
+                                    ]
+                                <|
+                                    cell <|
+                                        Element.el
+                                            [ Element.width <| Element.maximum 100 <| Element.fill ]
+                                        <|
+                                            Element.text <|
+                                                x.name
+                      }
+                    ]
+                }
+        ]
 
 
 type Message
@@ -138,6 +178,7 @@ type Message
 type MessagePure
     = SetSpaces (List { id : Int, name : String })
     | SetSelected (Maybe Int)
+    | OpenDialogToCreate
 
 
 updatePure : MessagePure -> Model -> Model
@@ -148,6 +189,9 @@ updatePure message model =
 
         SetSelected selection ->
             { model | selected = selection }
+
+        OpenDialogToCreate ->
+            { model | create = Just () }
 
 
 type MessageEffect
