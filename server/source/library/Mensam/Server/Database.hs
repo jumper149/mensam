36715ff1f7af57be38240ea05608681ea7bf4d34
Felix Springer <felixspringer149@gmail.com>
2023-05-02 19:10:27 +0200

    Try to use `validateTable`.

--- server/source/library/Mensam/Server/Database.hs
+++ server/source/library/Mensam/Server/Database.hs
@@ -49,6 +49,26 @@ checkDatabase ::
   ) =>
   m ()
 checkDatabase = void $ runSeldaTransactionT $ do
+--  -- TODO: The validator internally makes a mistake, thinking that INTEGER means Int32.
+--  -- https://github.com/valderman/selda/blob/ab9619db13b93867d1a244441bb4de03d3e1dadb/selda-sqlite/src/Database/Selda/SQLite.hs#L129
+--  lift $ logDebug "Validating table 'user'."
+--  Selda.validateTable tableUser
+--
+--  lift $ logDebug "Validating table 'session'."
+--  Selda.validateTable tableSession
+--
+--  lift $ logDebug "Validating table 'space'."
+--  Selda.validateTable tableSpace
+--
+--  lift $ logDebug "Validating table 'space_user'."
+--  Selda.validateTable tableSpaceUser
+--
+--  lift $ logDebug "Validating table 'desk'."
+--  Selda.validateTable tableDesk
+--
+--  lift $ logDebug "Validating table 'reservation'."
+--  Selda.validateTable tableReservation
+
   lift $ logInfo "Checking for overlapping reservations."
   reservationsOverlapping <- Selda.query $ do
     dbReservationX <- Selda.select tableReservation
