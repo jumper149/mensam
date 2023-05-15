b67b8919a22f4ef55f42a10d394a4f58b9b48a16
Felix Springer <felixspringer149@gmail.com>
2023-05-15 18:41:29 +0200

    Add "/reservation/cancel" to frontend API.

---
+++ frontend/source/Mensam/Api/ReservationCancel.elm
@@ -0,0 +1,108 @@
+module Mensam.Api.ReservationCancel exposing (..)
+
+import Http
+import Json.Decode as Decode
+import Json.Encode as Encode
+import Mensam.Auth.Bearer
+import Mensam.Reservation
+import Url.Builder
+
+
+type alias Request =
+    { jwt : Mensam.Auth.Bearer.Jwt
+    , id : Mensam.Reservation.Identifier
+    }
+
+
+type Response
+    = Success
+    | ErrorBody String
+    | ErrorAuth Mensam.Auth.Bearer.Error
+
+
+request : Request -> (Result Http.Error Response -> a) -> Cmd a
+request body handleResult =
+    Http.request
+        { method = "POST"
+        , headers =
+            [ Mensam.Auth.Bearer.authorizationHeader body.jwt
+            ]
+        , url =
+            Url.Builder.absolute
+                [ "api"
+                , "reservation"
+                , "cancel"
+                ]
+                []
+        , body = Http.jsonBody <| encodeBody body
+        , expect = Http.expectStringResponse handleResult responseResult
+        , timeout = Nothing
+        , tracker = Nothing
+        }
+
+
+responseResult : Http.Response String -> Result Http.Error Response
+responseResult httpResponse =
+    case httpResponse of
+        Http.BadUrl_ err ->
+            Err <| Http.BadUrl err
+
+        Http.Timeout_ ->
+            Err <| Http.Timeout
+
+        Http.NetworkError_ ->
+            Err <| Http.NetworkError
+
+        Http.BadStatus_ metadata body ->
+            case metadata.statusCode of
+                400 ->
+                    case Decode.decodeString decodeBody400 body of
+                        Ok error ->
+                            Ok <| ErrorBody error
+
+                        Err err ->
+                            Err <| Http.BadBody <| Decode.errorToString err
+
+                401 ->
+                    case Decode.decodeString Mensam.Auth.Bearer.http401BodyDecoder body of
+                        Ok error ->
+                            Ok <| ErrorAuth error
+
+                        Err err ->
+                            Err <| Http.BadBody <| Decode.errorToString err
+
+                status ->
+                    Err <| Http.BadStatus status
+
+        Http.GoodStatus_ metadata body ->
+            case metadata.statusCode of
+                200 ->
+                    case Decode.decodeString decodeBody200 body of
+                        Ok () ->
+                            Ok <| Success
+
+                        Err err ->
+                            Err <| Http.BadBody <| Decode.errorToString err
+
+                status ->
+                    Err <| Http.BadStatus status
+
+
+encodeBody : Request -> Encode.Value
+encodeBody body =
+    Encode.object
+        [ ( "id"
+          , Mensam.Reservation.identifierEncode body.id
+          )
+        ]
+
+
+decodeBody200 : Decode.Decoder ()
+decodeBody200 =
+    Decode.map (\_ -> ())
+        (Decode.field "unit" <| Decode.list <| Decode.succeed ())
+
+
+decodeBody400 : Decode.Decoder String
+decodeBody400 =
+    Decode.field "error" Decode.string
---
+++ frontend/source/Mensam/Reservation.elm
@@ -0,0 +1,24 @@
+module Mensam.Reservation exposing (..)
+
+import Json.Decode as Decode
+import Json.Encode as Encode
+
+
+type Identifier
+    = MkIdentifier Int
+
+
+identifierToString : Identifier -> String
+identifierToString (MkIdentifier identifier) =
+    String.fromInt identifier
+
+
+identifierEncode : Identifier -> Encode.Value
+identifierEncode (MkIdentifier identifier) =
+    Encode.int identifier
+
+
+identifierDecoder : Decode.Decoder Identifier
+identifierDecoder =
+    Decode.map MkIdentifier
+        Decode.int
