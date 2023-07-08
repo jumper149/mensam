3eca9478efabb308b7281bc7d83e80869dd43dd4
Felix Springer <felixspringer149@gmail.com>
2023-07-08 13:36:20 +0200

    Working App.

--- server/source/library/Elm.hs
+++ server/source/library/Elm.hs
@@ -4,18 +4,15 @@ module Elm where
 
 import Data.Kind
 
-f :: ()
-f = ()
+data Effect model = forall screen. MkEffect (model screen)
 
-data Effect message model = MkEffect (forall screen. model screen)
+newtype View message = MkView String
 
-newtype View message = MkView [message]
-
---data App model message = MkApp
---  { appModel :: model
---  , appUpdate :: message -> model -> Effect message model
---  , appView :: model -> View message
---  }
+data App (model :: screenKind -> Type) (message :: screenKind -> Type) = forall startScreen . MkApp
+  { appModel :: model startScreen
+  , appUpdate :: forall screen. message screen -> model screen -> Effect model
+  , appView :: forall screen. model screen -> View message
+  }
 
 data Screen = Screen1 | Screen2
 
@@ -26,11 +23,39 @@ data Model :: Screen -> Type where
 data Message :: Screen -> Type where
   Plus :: Int -> Message Screen1
   ReplaceChar :: Char -> Message Screen2
+  SwitchToScreen1 :: Message Screen2
+  Reset :: Message a
 
-update :: Message a -> Model a -> Effect (Message b) (Model b)
+update :: Message a -> Model a -> Effect Model
 update message model =
   case message of
     Plus n1 -> case model of
-                Screen1Model n2 -> MkEffect $ Screen1Model $ n1 + n2
-                Screen2Model _ -> undefined
-    ReplaceChar c -> undefined
+      Screen1Model n2 -> MkEffect $ Screen1Model $ n1 + n2
+    ReplaceChar c -> case model of
+      Screen2Model _ -> MkEffect $ Screen2Model c
+    SwitchToScreen1 -> case model of
+      Screen2Model _ -> MkEffect $ Screen1Model 0
+    Reset -> case model of
+      Screen1Model _ -> MkEffect $ Screen1Model 0
+      Screen2Model _ -> MkEffect $ Screen1Model 0
+
+view :: Model a -> View Message
+view model =
+  case model of
+    Screen1Model n ->
+      MkView $
+        unlines
+          [ "Screen1 with a number: " ++ show n
+          , "Add a number (TODO)"
+          ]
+    Screen2Model c -> MkView $ "Screen2 with a character: " ++ show c
+
+myApp :: App Model Message
+myApp = MkApp
+    { appModel = initial
+    , appUpdate = update
+    , appView = view
+    }
+
+initial :: Model Screen1
+initial = Screen1Model 5
