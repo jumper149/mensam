f7795b895d02992f4a262a554ac4a39e657fe16b
Felix Springer <felixspringer149@gmail.com>
2024-04-22 14:25:32 +0200

    Add `/space/delete` route to frontend API access.

---
+++ frontend/source/Mensam/Api/SpaceDelete.elm
@@ -0,0 +1,156 @@
+module Mensam.Api.SpaceDelete exposing (..)
+
+import Http
+import Http.Extra
+import Json.Decode as Decode
+import Json.Encode as Encode
+import Mensam.Auth.Bearer
+import Mensam.Space
+import Url.Builder
+
+
+type alias Request =
+    { jwt : Mensam.Auth.Bearer.Jwt
+    , id : Mensam.Space.Identifier
+    }
+
+
+type Response
+    = Success
+    | ErrorSpaceNotFound
+    | ErrorInsufficientPermission
+    | ErrorBody String
+    | ErrorAuth Mensam.Auth.Bearer.Error
+
+
+request : Request -> (Result Http.Error Response -> a) -> Cmd a
+request body handleResult =
+    Http.request
+        { method = "DELETE"
+        , headers =
+            [ Mensam.Auth.Bearer.authorizationHeader body.jwt
+            ]
+        , url =
+            Url.Builder.absolute
+                [ "api"
+                , "space"
+                , "delete"
+                ]
+                []
+        , body = Http.jsonBody <| encodeBody body
+        , expect = Http.expectStringResponse handleResult responseResult
+        , timeout = Nothing
+        , tracker = Http.Extra.tracker
+        }
+
+
+responseResult : Http.Response String -> Result Http.Error Response
+responseResult httpResponse =
+    case httpResponse of
+        Http.BadUrl_ err ->
+            Err <| Http.BadUrl err
+
+        Http.Timeout_ ->
+            Err <| Http.Timeout
+
+        Http.NetworkError_ ->
+            Err <| Http.NetworkError
+
+        Http.BadStatus_ metadata body ->
+            case metadata.statusCode of
+                400 ->
+                    case Decode.decodeString decodeBody400 body of
+                        Ok error ->
+                            Ok <| ErrorBody error
+
+                        Err err ->
+                            Err <| Http.BadBody <| Decode.errorToString err
+
+                401 ->
+                    case Decode.decodeString Mensam.Auth.Bearer.http401BodyDecoder body of
+                        Ok error ->
+                            Ok <| ErrorAuth error
+
+                        Err err ->
+                            Err <| Http.BadBody <| Decode.errorToString err
+
+                403 ->
+                    case Decode.decodeString decodeBody403 body of
+                        Ok () ->
+                            Ok <| ErrorInsufficientPermission
+
+                        Err err ->
+                            Err <| Http.BadBody <| Decode.errorToString err
+
+                404 ->
+                    case Decode.decodeString decodeBody404 body of
+                        Ok () ->
+                            Ok <| ErrorSpaceNotFound
+
+                        Err err ->
+                            Err <| Http.BadBody <| Decode.errorToString err
+
+                status ->
+                    Err <| Http.BadStatus status
+
+        Http.GoodStatus_ metadata body ->
+            case metadata.statusCode of
+                200 ->
+                    case Decode.decodeString decodeBody200 body of
+                        Ok () ->
+                            Ok <| Success
+
+                        Err err ->
+                            Err <| Http.BadBody <| Decode.errorToString err
+
+                status ->
+                    Err <| Http.BadStatus status
+
+
+encodeBody : Request -> Encode.Value
+encodeBody body =
+    Encode.object
+        [ ( "id"
+          , Mensam.Space.identifierEncode body.id
+          )
+        ]
+
+
+decodeBody200 : Decode.Decoder ()
+decodeBody200 =
+    Decode.map
+        (\_ -> ())
+        (Decode.field "unit" <| Decode.list <| Decode.succeed ())
+
+
+decodeBody400 : Decode.Decoder String
+decodeBody400 =
+    Decode.field "error" Decode.string
+
+
+decodeBody403 : Decode.Decoder ()
+decodeBody403 =
+    Decode.string
+        |> Decode.andThen
+            (\string ->
+                case string of
+                    "Insufficient permission." ->
+                        Decode.succeed ()
+
+                    _ ->
+                        Decode.fail <| "Unexpected HTTP 403 message: " ++ string
+            )
+
+
+decodeBody404 : Decode.Decoder ()
+decodeBody404 =
+    Decode.string
+        |> Decode.andThen
+            (\string ->
+                case string of
+                    "Space not found." ->
+                        Decode.succeed ()
+
+                    _ ->
+                        Decode.fail <| "Unexpected HTTP 404 message: " ++ string
+            )
